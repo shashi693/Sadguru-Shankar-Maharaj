bbfadaad2e0eff20f3dc76ef8936c75268dd2e7a
shashi693 <sukenkar@hotmail.com>
2016-12-17 18:47:42 +0530

    Compeleted
    back Button not applicable

---
+++ app/src/main/java/com/avenueinfotech/sadgurushankarmaharaj/fragments/MainFragment.java
@@ -0,0 +1,23 @@
+package com.avenueinfotech.sadgurushankarmaharaj.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.avenueinfotech.sadgurushankarmaharaj.R;
+
+/**
+ * Created by suken on 23-11-2016.
+ */
+
+public class MainFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main,container,false);
+        return rootView;
+    }
+}
